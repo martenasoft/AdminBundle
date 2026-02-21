bf0f5c3a46ecc6f1cdddc4a25840efaf2a1ba879
k.anisimov <k.anisimov@epicentrk.ua>
2026-02-21 16:39:37 +0200

    fix libs

--- src/Services/CountsOnMainService.php
+++ src/Services/CountsOnMainService.php
@@ -3,19 +3,19 @@
 namespace MartenaSoft\AdminBundle\Services;
 
 use MartenaSoft\AdminBundle\Dto\CountsOnMainDto;
-use MartenaSoft\PageBundle\Repository\PageRepository;
-use MartenaSoft\SiteBundle\Dto\ActiveSiteDto;
-use MartenaSoft\UserBundle\Repository\PermissionRepository;
-use MartenaSoft\UserBundle\Repository\RoleRepository;
-use MartenaSoft\UserBundle\Repository\UserRepository;
+//use MartenaSoft\PageBundle\Repository\PageRepository;
+use MartenaSoft\CommonLibrary\Dto\ActiveSiteDto;
+//use MartenaSoft\UserBundle\Repository\PermissionRepository;
+//use MartenaSoft\UserBundle\Repository\RoleRepository;
+//use MartenaSoft\UserBundle\Repository\UserRepository;
 
 class CountsOnMainService
 {
     public function __construct(
-        private readonly PageRepository $pageRepository,
-        private readonly UserRepository $userRepository,
-        private readonly RoleRepository $roleRepository,
-        private readonly PermissionRepository $permissionRepository,
+//        private readonly PageRepository $pageRepository,
+//        private readonly UserRepository $userRepository,
+//        private readonly RoleRepository $roleRepository,
+//        private readonly PermissionRepository $permissionRepository,
     ) {
     }
 
@@ -27,10 +27,10 @@ class CountsOnMainService
             'lang' => $lang,
         ];
         return new CountsOnMainDto(
-            pages: $this->pageRepository->count($criteriaWithLang),
-            users: $this->userRepository->count($criteria),
-            roles: $this->roleRepository->count($criteria),
-            permissions: $this->permissionRepository->count($criteria),
+//            pages: $this->pageRepository->count($criteriaWithLang),
+//            users: $this->userRepository->count($criteria),
+//            roles: $this->roleRepository->count($criteria),
+//            permissions: $this->permissionRepository->count($criteria),
         );
     }
 }
